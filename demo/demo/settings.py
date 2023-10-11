3c8c3dbe3555fc0055dfb0557af3857b69253c5a
gokuljs <gokuljs029@gmail.com>
2023-10-11 15:29:16 +0530

    django app

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "myapp"  # connecting django app(myapp) to the project
 ]
 
 MIDDLEWARE = [
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # this is basically connecting myapp the django app to the demo project(which is django project  )
+    # this is directly speak to the myapp/urls.py
+    path('', include("myapp.urls"))
 ]
